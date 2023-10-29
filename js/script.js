db83389189183b3f060f27db8d6fcfabe3426d37
Kenia Montalvo <kenlimonra@gmail.com>
2023-10-28 19:22:53 -0600

    acomodo de enlaces

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-/* barra de navegación */
+/* barra de navegación para todas las páginas*/
 
 const navbarContainer = document.getElementById('navbar-container');
 
-const navbarURL = 'navbar.html';
+const navbarURL = './navbar.html';
 
 fetch(navbarURL)
     .then(response => response.text())
@@ -12,13 +12,29 @@ fetch(navbarURL)
     .catch(error => {
         console.log("Error al cargar la barra de navegación", error);
     })
+
+
+/*barra de navegación para index*/
+
+const navbarContainerPrincipal = document.getElementById('navbar-container-principal');
+
+const navbarURLIndex = './html/navbarIndex.html';
+
+fetch(navbarURLIndex)
+    .then(response => response.text())
+    .then(data => {
+        navbarContainerPrincipal.innerHTML = data;
+    })
+    .catch(error => {
+        console.log("Error al cargar la barra de navegación", error);
+    })
     
 
-/* footer */
+/* footer para todas las páginas*/
 
 const footerContainer = document.getElementById('my-footer');
 
-const footerURL = 'footer.html';
+const footerURL = './footer.html';
 
 fetch(footerURL)
     .then(response => response.text())
@@ -29,4 +45,18 @@ fetch(footerURL)
         console.log("Error al cargar el footer", error);
     })
 
+/* footer para index*/
+
+const footerContainerPrincipal = document.getElementById('my-footer-principal');
+
+const footerURLIndex = './html/footerIndex.html';
+
+fetch(footerURLIndex)
+    .then(response => response.text())
+    .then(data => {
+        footerContainerPrincipal.innerHTML = data;
+    })
+    .catch(error => {
+        console.log("Error al cargar el footer", error);
+    })
 
